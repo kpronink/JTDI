d52819865a91601ab36505c8531117b6cc1bcd3c
kpronink <kpronink@gmail.com>
2017-10-24 10:57:31 +0500

    fix some bugs

--- JtdiTASKS/views.py
+++ JtdiTASKS/views.py
@@ -134,8 +134,8 @@ def get_index_tasks(request):
         count = 0
         for val in values:
             data.append({'y': str(val[0].day) + '.' + str(val[0].month),
-                         'a': values2[count][1],
-                         'b': val[1]})
+                         'b': values2[count][1],
+                         'a': val[1]})
             count += 1
     return JsonResponse(data, safe=False)
 
