ded1237b0d40fb0e9cc7355ca55af95ad348d2b5
kpronink <kpronink@gmail.com>
2017-10-26 13:09:58 +0500

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
