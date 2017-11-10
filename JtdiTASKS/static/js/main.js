fcf08c0d80e6af18ac1ad174f264157b6b9f4090
kpronink <kpronink@gmail.com>
2017-11-10 14:55:23 +0500

    fix bug

--- JtdiTASKS/static/js/main.js
+++ JtdiTASKS/static/js/main.js
@@ -259,7 +259,6 @@ $("#rename_proj").on("submit", ".rename_proj_form", function () {
   });
 
 $(document).ready(function(){
-    $('#modal-task').modal()
     var task_id = window.location.hash.replace("#","");
     if (task_id !== ''){
     TaskDetail("/task/det/"+task_id+"/")
