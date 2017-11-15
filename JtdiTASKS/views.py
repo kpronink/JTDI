38bea0d504095f42856459e3e6f803cc8d232ca3
kpronink <kpronink@gmail.com>
2017-11-15 12:06:53 +0500

    fix bugs

--- JtdiTASKS/views.py
+++ JtdiTASKS/views.py
@@ -174,9 +174,10 @@ def get_push_event(request):
     currentdate = datetime.datetime.today()
     start_day = currentdate.combine(currentdate, currentdate.min.time())
     local_timez = pytz.timezone(request.user.profile.timezone)
+    dt = datetime.datetime.now().astimezone(local_timez)
 
     tasks_today = QueueTask.objects.filter(reminded=False).filter(user=request.user)\
-        .filter(date_time__range=(start_day, currentdate.astimezone(local_timez)))\
+        .filter(date_time__range=(start_day, currentdate))\
         .order_by('date_time').reverse()
     count = 0
     for task_actual in tasks_today:
