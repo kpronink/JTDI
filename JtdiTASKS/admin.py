905067fa7278de2c56b8e5ac11623a1cb5ebb50c
kpronink <kpronink@gmail.com>
2017-12-01 10:38:14 +0500

    fix bugs

--- JtdiTASKS/admin.py
+++ JtdiTASKS/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Task, Profile, Project, InviteUser
+from .models import Task, Profile, Project, InviteUser, KanbanStatus
 
 admin.site.register(Task)
 admin.site.register(Profile)
 admin.site.register(Project)
 admin.site.register(InviteUser)
+admin.site.register(KanbanStatus)
